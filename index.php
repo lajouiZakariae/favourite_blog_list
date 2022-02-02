cba6c3b15c5bd9f10dca7747c4a968c57f5c2449
Zakariae Lajoui <80148036+lajouiZakariae@users.noreply.github.com>
2022-02-02 01:04:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<div class="container-md mt-2">
+  <div class="row">
+  
+        <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-3">
+          <div class="card post mx-auto">
+
+              <img src="..." class="img-fluid rounded-top" alt="">
+
+              <div class="card-body">
+                <h5 class="post-title text-capitalize">
+                  <a href="">Link</a>
+                </h5>
+                <div class="post-info">
+                  
+                </div>
+                <div class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, ipsa non totam ipsam ratione hic nam expedita cum consectetur, a impedit, aut eius quibusdam...
+                </div>
+              </div>
+
+              <div class="card-footer">
+                <span class="mb-0 d-block d-md-inline d-xl-block">
+                  <i class="fas fa-tag"></i>
+                    Category
+                </span>
+                <span class="post-date float-md-right float-xl-left" >
+                    <i class="fas fa-calendar"></i> January 12, 2022
+                  </span>
+              </div>
+
+            </div>
+        </div>
+  </div>
+</div>
